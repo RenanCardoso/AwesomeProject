a68b490c229d12e6ff18b3859dd103d6a5bb1f4f
RenanCardoso <renancardoso2401@gmail.com>
2025-10-08 10:34:54 -0300

    Ajustes eslint e gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,23 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-native', 'import'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-native/all',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  env: {
+    'react-native/react-native': true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
